ab44d8becf3ee5f4187a61b1ced969ecd9ec1adb
PitchLost <thundergames221@gmail.com>
2024-03-13 10:24:45 +1300

    Add to cart Button

---
+++ L3_Programming/L3_cart/client/cart.js
@@ -0,0 +1,13 @@
+// Shopping cart code: 
+console.log('Shopping cart code loaded')
+// Define cart variables: 
+let cart_items = []
+let item_counter = 0
+let item_added = { 
+    cart_price: '',
+    cart_name: ''
+}
+
+function add_cart() { 
+    
+}
